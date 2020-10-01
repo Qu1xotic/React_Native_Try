5e38adee73c725b3850e43179b7612607c355778
Qu1xotic <56445746+Qu1xotic@users.noreply.github.com>
2020-10-01 19:48:00 +0530

    init

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View,} from 'react-native'
+import SOSButton from './components/SOSButton'
+import Header from './components/Header'
+export default class App extends Component {
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.head} >
+          <Header />
+        </View>
+        <View style={{alignItems:'center',justifyContent:'center',height:'100%'}}>
+          <SOSButton />
+        </View>
+      </View>
+
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    marginTop:23,
+  },
+  head:{
+    marginTop:20,
+  }
+})
